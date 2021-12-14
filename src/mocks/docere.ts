2f6468e38d3e7add1fe2b51b25a7d5e58fb9fdd2
LuisaBC-pixel <bc.luisa@yahoo.com.br>
2021-12-14 17:17:08 -0300

    create mocks decere

---
+++ src/mocks/docere.ts
@@ -0,0 +1,31 @@
+module.exports = [
+  {
+    title: 'Docere',
+    subTitle: 'para quando precisar',
+    description:
+      'Necessita de ajuda para entender qualquer matéria? Acessa Docere, é de graça!',
+    image:
+      'https://docere.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FDocere-logo.24d070a5.png&w=256&q=75',
+    url: 'https://docere.vercel.app/',
+    aboutTitle: 'Uma maneira de espalhar a educação para todos!',
+    aboutDescription:
+      'É um aplicativo que além de capacitar os pais a ensinarem aos seus filhos, ensina para qualquer um! O aplicativo é inclusivo para todos, tem legendas nos vídeos, tem ferramenta para libras, tem ferramenta para passar o texto em áudio. Tudo isso desde a abertura do aplicativo até o fechamento do aplicativo. Nele vai ter diversos tipos de assuntos. Desde ensinar a escrever e ler, fazer contas matemáticas, programação, finanças, história, geografia, economia doméstica e entre outros. O usuário escolhe qual(is) matéria(s) ele deseja cursar. Quando o usuário escolher a matéria e clicar nele para aprender, o aplicativo vai mostrar os vídeos de professores ensinando (legendado).Vai mostrar textos (parecido com uma apostila) e permite o aluno imprimir, grifar no próprio app. Durante o percursos das aulas vão ter pequenos desafios para os alunos fazerem. (nota: Os desafios não vão definir se o aluno foi aprovado na matéria, não vai ter essa aprovação e reprovação)',
+    details: [
+      {
+        title: 'Estou com dúvida de uma matéria, o que fazer?',
+        description:
+          'Entra no fórum e manda a sua pergunta, qualquer um pode responder (lembrando que, se você responder a dúvida do outro(a) aluno(a), aumenta mais pontos para sua conta, ainda mais se tiver votação para 👍).',
+      },
+      {
+        title: 'Não tenho internet na minha casa, o que fazer?',
+        description:
+          'Vai ter lugares financiado pelo próprio Docere para que você consiga estudar usando computadores. Terão profissionais disposta a te ajudar com o que precisar.',
+      },
+      {
+        title: 'Vai ter pontuações no aplicativo?',
+        description:
+          'Vai ter, mas isso não vai definir se você é mais inteligente ou se foi aprovado. As pontuações vão servir como impulso para você se dedicar mais ao estudo e compartilhar conhecimento.',
+      },
+    ],
+  },
+];
