3b9f18a9a63d0e5140b7e29741cd2aee57d4b3a5
LuisaBC-pixel <bc.luisa@yahoo.com.br>
2021-12-15 13:34:09 -0300

    create routes and server

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
-const express = require('express');
+import express from 'express';
+import { router } from './routes';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-})
+app.use(express.json());
+app.use(router);
 
-app.listen(3000, console.log('http://localhost:3000'));
+export { app };
